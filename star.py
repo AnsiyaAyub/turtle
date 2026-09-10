348f4428f64b440b54555d3c7d283d6f1529e649
AnsiyaAyub <AnsiyaShefeeque@gmail.com>
2026-09-10 19:23:46 +0400

    Add files via upload

---
+++ star.py
@@ -0,0 +1,28 @@
+import turtle
+draw = turtle.Turtle()
+
+turtle.Screen().bgcolor("Aqua")
+
+draw.forward(100)
+
+draw.left(120)
+draw.forward(100)
+
+draw.left(120)
+draw.forward(100)
+
+draw.penup()
+draw.right(150)
+draw.forward(50)
+
+draw.pendown()
+draw.right(90)
+draw.forward(100)
+
+draw.right(120)
+draw.forward(100)
+
+draw.right(120)
+draw.forward(100)
+
+turtle.done()
